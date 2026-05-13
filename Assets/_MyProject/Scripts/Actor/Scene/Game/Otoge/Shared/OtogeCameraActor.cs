8d83803df04fa0a51a90b3a7657b276d1457ecd9
kageki128 <kagemakopf@gmail.com>
2026-05-14 00:41:06 +0900

    カメラも対象にした

---
+++ Assets/_MyProject/Scripts/Actor/Scene/Game/Otoge/Shared/OtogeCameraActor.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace MyProject.Actor
+{
+    public class OtogeCameraActor : OtogeSharedActorBase
+    {
+        [Serializable]
+        class OtogeCameraSettings
+        {
+            public OtogeType Type => type;
+            [SerializeField] OtogeType type;
+
+            public Vector3 LocalPosition => localPosition;
+            [SerializeField] Vector3 localPosition;
+
+            public Vector3 LocalEulerAngles => localEulerAngles;
+            [SerializeField] Vector3 localEulerAngles;
+        }
+
+        [Serializable]
+        class DefaultCameraSettings
+        {
+            public Vector3 LocalPosition => localPosition;
+            [SerializeField] Vector3 localPosition;
+
+            public Vector3 LocalEulerAngles => localEulerAngles;
+            [SerializeField] Vector3 localEulerAngles;
+        }
+
+        [SerializeField] OtogeCameraSettings[] otogeCameraSettings;
+        [SerializeField] DefaultCameraSettings defaultSettings;
+
+        public override void Initialize()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public override UniTask ShowAsync(CancellationToken ct)
+        {
+            gameObject.SetActive(true);
+            return UniTask.CompletedTask;
+        }
+
+        public override UniTask HideAsync(CancellationToken ct)
+        {
+            gameObject.SetActive(false);
+            return UniTask.CompletedTask;
+        }
+
+        public override void SetState(OtogeType otogeType)
+        {
+            var settings = Array.Find(otogeCameraSettings, x => x.Type == otogeType);
+
+            transform.localPosition = settings?.LocalPosition ?? defaultSettings.LocalPosition;
+            transform.localEulerAngles = settings?.LocalEulerAngles ?? defaultSettings.LocalEulerAngles;
+        }
+    }
+}
