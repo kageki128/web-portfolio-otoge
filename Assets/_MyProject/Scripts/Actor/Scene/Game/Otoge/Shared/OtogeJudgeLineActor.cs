45c3c43d7e40ca32ec82ab0d35437e23866358ed
kageki128 <kagemakopf@gmail.com>
2026-05-16 23:44:14 +0900

    JudgeLineを湾曲できるようにした

---
+++ Assets/_MyProject/Scripts/Actor/Scene/Game/Otoge/Shared/OtogeJudgeLineActor.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace MyProject.Actor
+{
+    [RequireComponent(typeof(LineRenderer))]
+    public class OtogeJudgeLineActor : OtogeSharedActorBase
+    {
+        [Serializable]
+        class OtogeJudgeLineSettings
+        {
+            public OtogeType Type => type;
+            [SerializeField] OtogeType type;
+
+            public Vector3 LocalPosition => localPosition;
+            [SerializeField] Vector3 localPosition;
+
+            public Vector3 LocalEulerAngles => localEulerAngles;
+            [SerializeField] Vector3 localEulerAngles;
+
+            public float LineLength => lineLength;
+            [SerializeField] float lineLength;
+
+            public float CurveRatePercent => curveRatePercent;
+            [SerializeField, Range(0f, 100f)] float curveRatePercent;
+        }
+
+        [SerializeField] OtogeJudgeLineSettings[] otogeJudgeLineSettings;
+        [SerializeField, Min(2)] int curveSegments = 128;
+
+        public override void Initialize()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public override UniTask ShowAsync(CancellationToken ct)
+        {
+            gameObject.SetActive(true);
+            return UniTask.CompletedTask;
+        }
+
+        public override UniTask HideAsync(CancellationToken ct)
+        {
+            gameObject.SetActive(false);
+            return UniTask.CompletedTask;
+        }
+
+        public override void SetState(OtogeType otogeType)
+        {
+            var lineSettings = Array.Find(otogeJudgeLineSettings, x => x.Type == otogeType);
+            if (lineSettings == null)
+            {
+                gameObject.SetActive(false);
+                return;
+            }
+
+            gameObject.SetActive(true);
+            var lineRenderer = GetComponent<LineRenderer>();
+            transform.localPosition = lineSettings.LocalPosition;
+            transform.localEulerAngles = lineSettings.LocalEulerAngles;
+
+            var curveRate = Mathf.Clamp01(lineSettings.CurveRatePercent / 100f);
+            if (curveRate == 0f)
+            {
+                lineRenderer.positionCount = 2;
+                var halfLength = lineSettings.LineLength / 2f;
+                lineRenderer.SetPosition(0, new Vector3(-halfLength, 0f, 0f));
+                lineRenderer.SetPosition(1, new Vector3(halfLength, 0f, 0f));
+                return;
+            }
+
+            var arcAngle = Mathf.Lerp(0.0001f, Mathf.PI * 2f, curveRate);
+            var radius = lineSettings.LineLength / arcAngle;
+            var halfArcAngle = arcAngle / 2f;
+
+            var points = Mathf.Max(2, curveSegments);
+            lineRenderer.positionCount = points + 1;
+
+            for (var i = 0; i <= points; i++)
+            {
+                var t = i / (float)points;
+                var angle = Mathf.Lerp(-halfArcAngle, halfArcAngle, t);
+                var x = radius * Mathf.Sin(angle);
+                var y = radius - radius * Mathf.Cos(angle);
+                lineRenderer.SetPosition(i, new Vector3(x, y, 0f));
+            }
+        }
+    }
+}
