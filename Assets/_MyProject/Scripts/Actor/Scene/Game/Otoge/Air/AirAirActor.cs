6c620ccc1b045ae2b9049380967912020e5706df
kageki128 <kagemakopf@gmail.com>
2026-05-22 20:43:45 +0900

    Airをビルボードにした

--- Assets/_MyProject/Scripts/Actor/Scene/Game/Otoge/Air/AirAirActor.cs
+++ Assets/_MyProject/Scripts/Actor/Scene/Game/Otoge/Air/AirAirActor.cs
@@ -1,6 +1,56 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using MyProject.Core;
+using UnityEngine;
+
 namespace MyProject.Actor
 {
-    public class AirAirActor : AirTapActor
+    public class AirAirActor : NoteActorBase
     {
+        [SerializeField] SpriteRenderer image;
+
+        public override void Initialize()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public override UniTask ShowAsync(CancellationToken ct)
+        {
+            gameObject.SetActive(true);
+            SetAppearance(NoteCore.State.CurrentValue);
+            return UniTask.CompletedTask;
+        }
+
+        public override UniTask HideAsync(CancellationToken ct)
+        {
+            gameObject.SetActive(false);
+            return UniTask.CompletedTask;
+        }
+
+        public override void SetPosition(float currentBeat, float currentScroll, float scrollSpeed)
+        {
+            float x = CalculateCenterX(NoteCore.Property.Lane, NoteCore.Property.Width);
+            float y = (NoteCore.Property.ScrollBegin - currentScroll) * scrollSpeed;
+            transform.localPosition = new Vector3(x, y, 0);
+
+            var euler = image.transform.eulerAngles;
+            euler.x = Camera.main.transform.eulerAngles.x;
+            image.transform.rotation = Quaternion.Euler(euler);
+        }
+
+        protected override void SetWidth(int width)
+        {
+            image.size = new Vector2(width, image.size.y);
+        }
+
+        protected override void SetLayer(int layer)
+        {
+            image.sortingOrder = layer;
+        }
+
+        protected override void SetAppearance(NoteState state)
+        {
+            gameObject.SetActive(state is not NoteState.AfterJudge);
+        }
     }
 }
