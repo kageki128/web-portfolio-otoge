51047d067115a03a4297b4d0726e77c16a1f76f4
kageki128 <kagemakopf@gmail.com>
2026-06-02 18:01:06 +0900

    キーのスケールアニメーションをやめた

--- Assets/_MyProject/Scripts/Actor/Scene/Game/Otoge/LaneKeysActor.cs
+++ Assets/_MyProject/Scripts/Actor/Scene/Game/Otoge/LaneKeysActor.cs
@@ -12,31 +12,23 @@ namespace MyProject.Actor
         [SerializeField] List<TMP_Text> laneKeyTexts;
         [SerializeField] TMP_Text airKeyText;
         [SerializeField] SpriteRenderer airKeyRenderer;
-        [SerializeField] Transform airKeyParent;
 
         const float FadeDuration = 0.033f;
         const float ActiveAlpha = 1f;
-        const float ActiveScaleMultiplier = 1.1f;
 
         MotionHandle[] laneColorHandles;
-        MotionHandle[] laneScaleHandles;
         MotionHandle airTextHandle;
         MotionHandle airRendererHandle;
-        MotionHandle airScaleHandle;
         Color[] baseLaneColors;
-        Vector3[] baseLaneScales;
         Color baseAirTextColor;
         Color baseAirRendererColor;
-        Vector3 baseAirScale;
 
         public override void Initialize()
         {
             baseLaneColors = new Color[laneKeyTexts.Count];
-            baseLaneScales = new Vector3[laneKeyTexts.Count];
             for (var i = 0; i < laneKeyTexts.Count; i++)
             {
                 baseLaneColors[i] = laneKeyTexts[i].color;
-                baseLaneScales[i] = laneKeyTexts[i].transform.localScale;
             }
 
             if (airKeyText != null)
@@ -49,13 +41,7 @@ namespace MyProject.Actor
                 baseAirRendererColor = airKeyRenderer.color;
             }
 
-            if (airKeyParent != null)
-            {
-                baseAirScale = airKeyParent.localScale;
-            }
-
             laneColorHandles = new MotionHandle[laneKeyTexts.Count];
-            laneScaleHandles = new MotionHandle[laneKeyTexts.Count];
             gameObject.SetActive(false);
         }
 
@@ -70,7 +56,6 @@ namespace MyProject.Actor
             for (var i = 0; i < laneKeyTexts.Count; i++)
             {
                 laneKeyTexts[i].color = baseLaneColors[i];
-                laneKeyTexts[i].transform.localScale = baseLaneScales[i];
             }
 
             if (airKeyText != null)
@@ -83,11 +68,6 @@ namespace MyProject.Actor
                 airKeyRenderer.color = baseAirRendererColor;
             }
 
-            if (airKeyParent != null)
-            {
-                airKeyParent.localScale = baseAirScale;
-            }
-
             gameObject.SetActive(true);
             return UniTask.CompletedTask;
         }
@@ -97,19 +77,15 @@ namespace MyProject.Actor
             if (lane < 0 || lane >= laneKeyTexts.Count) return;
 
             laneColorHandles[lane].TryCancel();
-            laneScaleHandles[lane].TryCancel();
             laneKeyTexts[lane].color = WithAlpha(baseLaneColors[lane], ActiveAlpha);
-            laneKeyTexts[lane].transform.localScale = baseLaneScales[lane] * ActiveScaleMultiplier;
         }
 
         public void LightUpAir()
         {
             airTextHandle.TryCancel();
             airRendererHandle.TryCancel();
-            airScaleHandle.TryCancel();
             airKeyText.color = WithAlpha(baseAirTextColor, ActiveAlpha);
             airKeyRenderer.color = WithAlpha(baseAirRendererColor, ActiveAlpha);
-            airKeyParent.localScale = baseAirScale * ActiveScaleMultiplier;
         }
 
         public void LightDownLane(int lane)
@@ -117,19 +93,15 @@ namespace MyProject.Actor
             if (lane < 0 || lane >= laneKeyTexts.Count) return;
 
             laneColorHandles[lane].TryCancel();
-            laneScaleHandles[lane].TryCancel();
             laneColorHandles[lane] = FadeToInactive(laneKeyTexts[lane], baseLaneColors[lane]);
-            laneScaleHandles[lane] = FadeToBaseScale(laneKeyTexts[lane].transform, baseLaneScales[lane]);
         }
 
         public void LightDownAir()
         {
             airTextHandle.TryCancel();
             airRendererHandle.TryCancel();
-            airScaleHandle.TryCancel();
             airTextHandle = FadeToInactive(airKeyText, baseAirTextColor);
             airRendererHandle = FadeToInactive(airKeyRenderer, baseAirRendererColor);
-            airScaleHandle = FadeToBaseScale(airKeyParent, baseAirScale);
         }
 
         MotionHandle FadeToInactive(TMP_Text text, Color baseColor)
@@ -143,13 +115,6 @@ namespace MyProject.Actor
         {
             return LMotion.Create(renderer.color, baseColor, FadeDuration)
                 .Bind(value => renderer.color = value)
-                .AddTo(this);
-        }
-
-        MotionHandle FadeToBaseScale(Transform target, Vector3 baseScale)
-        {
-            return LMotion.Create(target.localScale, baseScale, FadeDuration)
-                .Bind(value => target.localScale = value)
                 .AddTo(this);
         }
 
